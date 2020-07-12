fc1dd946566050b435932357bcfd91495106759e
Christopher Lang <christopher-d-lang@hotmail.com>
2020-07-12 10:29:09 +0100

    Added object type and id sellection to server side network objects.

--- Chicken VS Chicken Game Server/src/Server/NetworkObject.cs
+++ Chicken VS Chicken Game Server/src/Server/NetworkObject.cs
@@ -6,14 +6,49 @@ using System.Threading.Tasks;
 
 namespace GameServer
 {
+    enum NetworkObjectType
+    {
+        player,
+        wall,
+        testObject
+    }
     abstract class NetworkObject
     {
         private static List<NetworkObject> allNetworkObjects = new List<NetworkObject>();
+
+        public readonly short objectId;
+        public readonly short objectTypeId;
+
         private readonly Component[] components;
-        public NetworkObject(Component[] _components)
+
+        public NetworkObject(NetworkObjectType _ojectType, Component[] _components)
         {
             allNetworkObjects.Add(this);
             components = _components;
+            objectTypeId = (short)_ojectType;
+
+            // Sellect object id that is not in use.
+
+            List<short> _takenObjectIds = new List<short>();
+
+            foreach (NetworkObject _object in allNetworkObjects)
+            {
+                _takenObjectIds.Add(_object.objectId);
+            }
+
+            for (short i = 0; i <= short.MaxValue; i++)
+            {
+                if (!_takenObjectIds.Contains(i))
+                {
+                    objectId = i;
+                    break;
+                }
+            }
+            if (objectId == short.MaxValue)
+            {
+                Console.WriteLine($"CRITICAL WARNING : max NetworkSynchroniser count reached ({short.MaxValue}) reached - ignoring this NetworkSynchroniser");
+                return;
+            }
         }
 
         // This method is virtual so component update order can be overridden.
